7cbb16ba8428f830bda03d0fc8cf07c96f53a6c8
Daniel Berthereau <Daniel.git@Berthereau.net>
2023-01-09 00:00:00 +0000

    Used Omeka S v4 feature "element group" for settings.

--- src/Form/SettingsFieldset.php
+++ src/Form/SettingsFieldset.php
@@ -1,4 +1,5 @@
 <?php declare(strict_types=1);
+
 namespace ViewerJs\Form;
 
 use Laminas\Form\Fieldset;
@@ -8,14 +9,24 @@ class SettingsFieldset extends Fieldset
 {
     protected $label = 'Viewer JS'; // @translate
 
+    protected $elementGroups = [
+        // "Player" is used instead of viewer, because "viewer" is used for a site
+        // user role and cannot be translated differently (no context).
+        // Player is polysemic too anyway, but less used and more adapted for
+        // non-image viewers.
+        'player' => 'Players', // @translate
+    ];
+
     public function init(): void
     {
         $this
             ->setAttribute('id', 'viewer-js')
+            ->setOption('element_groups', $this->elementGroups)
             ->add([
                 'name' => 'viewerjs_source_property',
                 'type' => PropertySelect::class,
                 'options' => [
+                    'element_group' => 'player',
                     'label' => 'Property used for external file', // @translate
                     'info' => 'The property supplying the file via URL, for example "dcterms:hasFormat" or "dcterms:isFormatOf".', // @translate
                     'empty_option' => '',
