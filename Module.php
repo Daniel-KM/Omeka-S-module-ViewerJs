241b0a931c2e56b8ae3b05b965e6e9f805dac2af
Daniel Berthereau <Daniel.git@Berthereau.net>
2025-03-03 00:00:00 +0000

    Updated Common version.

--- Module.php
+++ Module.php
@@ -46,7 +46,7 @@ use Omeka\Module\AbstractModule;
  * Display standard document formats (pdf and office: pdf, odt, ods, odp) in a
  * light unified player.
  *
- * @copyright Daniel Berthereau, 2017-2024
+ * @copyright Daniel Berthereau, 2017-2025
  * @license http://www.cecill.info/licences/Licence_CeCILL_V2.1-en.txt
  */
 class Module extends AbstractModule
@@ -61,10 +61,10 @@ class Module extends AbstractModule
         $translate = $services->get('ControllerPluginManager')->get('translate');
         $translator = $services->get('MvcTranslator');
 
-        if (!method_exists($this, 'checkModuleActiveVersion') || !$this->checkModuleActiveVersion('Common', '3.4.60')) {
+        if (!method_exists($this, 'checkModuleActiveVersion') || !$this->checkModuleActiveVersion('Common', '3.4.66')) {
             $message = new \Omeka\Stdlib\Message(
                 $translate('The module %1$s should be upgraded to version %2$s or later.'), // @translate
-                'Common', '3.4.60'
+                'Common', '3.4.66'
             );
             throw new \Omeka\Module\Exception\ModuleCannotInstallException((string) $message);
         }
